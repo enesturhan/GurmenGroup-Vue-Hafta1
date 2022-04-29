600d5f9ce94b904601b6ebf3a01bb4d1124fcb42
enes turhan <enesturhan9@gmail.com>
2022-04-30 01:00:40 +0300

    update

---
+++ week-1/dbet-coupon-app/src/components/KuponList.vue
@@ -0,0 +1,19 @@
+<script>
+import KuponItem from "@/components/KuponItem";
+export default {
+  inject: ["provideData"],
+    components: {
+        KuponItem
+    }
+}
+</script>
+
+<template>
+
+          <div class="card my-coupon">
+            <div class="card--header">
+ <h4>Kuponum</h4>
+            </div>
+            <KuponItem v-for="item in provideData.coupons" :key="item.id" :item="item"/>
+          </div>
+</template>
